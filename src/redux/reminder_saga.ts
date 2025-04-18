2752516e91a94488f60561738a18b051d196f2ef
bidhan-skybase <bidhan@skybase.com.np>
2025-04-18 15:24:24 +0545

    feat: Add snackbar in saga

--- src/redux/reminder_saga.ts
+++ src/redux/reminder_saga.ts
@@ -1,27 +1,37 @@
-import { takeEvery, put, call } from 'redux-saga/effects';
-import { PayloadAction } from '@reduxjs/toolkit';
-import { MedicineEntry } from '../model/reminder_model';
-import { storeReminder } from '../helpers/storage_helpers';
+import {takeEvery, put, call} from 'redux-saga/effects';
+import {PayloadAction} from '@reduxjs/toolkit';
+import {MedicineEntry} from '../model/reminder_model';
+import {storeReminder} from '../helpers/storage_helpers';
 import {
-    addReminderStart,
-    addReminderSuccess,
-    addReminderFailure,
+  addReminderStart,
+  addReminderSuccess,
+  addReminderFailure,
 } from '../redux/reminder_slice';
-import {SaveStatus} from "../type.ts";
+import {SaveStatus} from '../type.ts';
+import Snackbar from 'react-native-snackbar';
 
 function* addReminderSaga(action: PayloadAction<MedicineEntry>) {
-    try {
-        const result: SaveStatus = yield call(storeReminder, action.payload);
-        if (result.success) {
-            yield put(addReminderSuccess(action.payload));
-        } else {
-            yield put(addReminderFailure(result.message));
-        }
-    } catch (error) {
-        yield put(addReminderFailure('An unexpected error occurred'));
+  try {
+    const result: SaveStatus = yield call(storeReminder, action.payload);
+    if (result.success) {
+      yield put(addReminderSuccess(action.payload));
+      Snackbar.show({
+        text: 'Successfully added reminder',
+      });
+    } else {
+      yield put(addReminderFailure(result.message));
+      Snackbar.show({
+        text: 'Successfully could not be added reminder',
+      });
     }
+  } catch (error) {
+    Snackbar.show({
+      text: 'Successfully could not be added reminder',
+    });
+    yield put(addReminderFailure('An unexpected error occurred'));
+  }
 }
 
 export default function* remindersSaga() {
-    yield takeEvery(addReminderStart.type, addReminderSaga);
+  yield takeEvery(addReminderStart.type, addReminderSaga);
 }
