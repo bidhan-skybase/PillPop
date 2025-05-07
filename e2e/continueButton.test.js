19871ec78857bd226cb4bcebe2eaae7edaa0cbbf
bidhan-skybase <bidhan@skybase.com.np>
2025-05-07 16:31:01 +0545

    fix: Test time out time

--- e2e/continueButton.test.js
+++ e2e/continueButton.test.js
@@ -10,9 +10,7 @@ describe('Button Press Test', () => {
     it('should find and tap the Continue button', async () => {
         // Wait for the Continue button to be visible
         await waitFor(element(by.id('continue_button')))
-            .toBeVisible()
-            .withTimeout(15000); // Increased timeout for CI
-
+            .toBeVisible();
         // Tap the Continue button
         await element(by.id('continue_button')).tap();
 
